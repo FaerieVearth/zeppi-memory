447e174f9430f227dabfa60fcfaf05b9b4612f54
FaerieVearth <valentin.slayer@gmail.com>
2019-10-26 22:01:47 +0200

    #implemented moves and timer logic

--- js/main.js
+++ js/main.js
@@ -3,20 +3,41 @@ window.onload = function(){
 }
 
 let comparedSquares = [];
+let movesCount = 0;
+let tmie = 0;
+let startFlag = false;
+let numberOfPairsLeft;
+let movesElement;
+let gameOver = false;
+
 
 function init(){
-    console.log("Zeppo initialized");
+    numberOfPairsLeft = $(".square").length / 2;
 
-    let movesCount = 0;
-    let movesElement = document.querySelector("score-moves");
+    movesElement = document.querySelector(".score-moves");
+
+    movesElement.innerHTML = "Moves: " + movesCount;
 
     clearArray(comparedSquares);
 
     shuffleSquares();
     resetSquareState();
 
-    $(".square").on("mouseup", function(){
+    $(".square").on("click", function(el){
+        if(gameOver){
+            return;
+        }
+        if(!startFlag){
+            startFlag = true;
+            startTimer();
+        }
+
         let selectedElement = (this).children[0];
+
+        if(selectedElement.classList.contains("found")){
+            return;
+        }
+
         comparedSquares.forEach(function(e){
             if(e === selectedElement){
                 return;
@@ -27,19 +48,26 @@ function init(){
             selectedElement.classList.toggle("hidden");
             comparedSquares.push(selectedElement);
         }
-
-        console.log(comparedSquares);
         
         if(comparedSquares.length == 2){
             if(comparedSquares[0].isEqualNode(comparedSquares[1])){
-                console.log("foud");
+                movesCount++;
+                movesElement.innerHTML = "Moves: " + movesCount;
                 comparedSquares.forEach(function(e){
                     e.classList.add("found");
                     e.classList.remove("hidden");
                 });
                 clearArray(comparedSquares);
+                numberOfPairsLeft--;
+                console.log(numberOfPairsLeft);
+                if(numberOfPairsLeft === 0){
+                    stopTimer();
+                    gameOver = true;
+                }
+
             }else{
-                console.log("nah");
+                movesCount++;
+                movesElement.innerHTML = "Moves: " + movesCount;
                 setTimeout(function(){
                     comparedSquares.forEach(function(e){
                         e.classList.add("hidden");
@@ -52,6 +80,12 @@ function init(){
 }
 
 function restart(){
+    gameOver = false;
+    movesCount = 0;
+    startFlag = false;
+    numberOfPairsLeft = $(".square").length / 2;
+    movesElement.innerHTML = "Moves: " + movesCount;
+    resetTimer();
     shuffleSquares();
     resetSquareState();
     clearArray(comparedSquares);
@@ -71,7 +105,6 @@ class Target {
 }
 
 function shuffleSquares(){
-    console.log("shuffle them Zeppis");
     var squares = $(".square");
     for(var i = 0; i < squares.length; i++){
         var target = Math.floor(Math.random() * squares.length -1) + 1;
@@ -81,10 +114,54 @@ function shuffleSquares(){
 }
 
 function resetSquareState(){
-    console.log("reset zeppi state");
     var squares = $(".square");
     for(var i = 0; i < squares.length; i++){
         squares[i].children[0].classList.remove("selected", "found", "hidden");
         squares[i].children[0].classList.add("hidden");
     }
 }
+
+var timeBegan = null
+    , timeStopped = null
+    , stoppedDuration = 0
+    , started = null;
+
+function startTimer() {
+    if (timeBegan === null) {
+        timeBegan = new Date();
+    }
+
+    if (timeStopped !== null) {
+        stoppedDuration += (new Date() - timeStopped);
+    }
+
+    started = setInterval(clockRunning, 10);	
+}
+
+function stopTimer() {
+    timeStopped = new Date();
+    clearInterval(started);
+}
+ 
+function resetTimer() {
+    clearInterval(started);
+    stoppedDuration = 0;
+    timeBegan = null;
+    timeStopped = null;
+    document.getElementById("display-area").innerHTML = "Timer: 00:00:00.000";
+}
+
+function clockRunning(){
+    var currentTime = new Date()
+        , timeElapsed = new Date(currentTime - timeBegan - stoppedDuration)
+        , hour = timeElapsed.getUTCHours()
+        , min = timeElapsed.getUTCMinutes()
+        , sec = timeElapsed.getUTCSeconds()
+        , ms = timeElapsed.getUTCMilliseconds();
+
+    document.getElementById("display-area").innerHTML = "Timer: " +
+        (hour > 9 ? hour : "0" + hour) + ":" + 
+        (min > 9 ? min : "0" + min) + ":" + 
+        (sec > 9 ? sec : "0" + sec) + "." + 
+        (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms);
+};
